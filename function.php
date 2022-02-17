0958535e8c82d9d184dc64f8e33e01c0117c1127
okyach <99065870+okyach@users.noreply.github.com>
2022-02-17 07:13:09 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,7 +1,7 @@
 <?php
 function request($url, $token = null, $data = null, $pin = null, $otpsetpin = null, $uuid = null){
 $header[] = "Host: api.gojekapi.com";
-$header[] = "User-Agent: okhttp/3.10.0";
+$header[] = "User-Agent: okhttp/4.9.1";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: id-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
